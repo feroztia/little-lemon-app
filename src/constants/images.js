5f4bfc902cf91c152a8fcf02cbbdd660f96eb6d3
valeriia hashemi <vzakras@gmail.com>
2023-07-08 14:57:30 +0200

    Addedall folders

---
+++ src/constants/images.js
@@ -0,0 +1,17 @@
+import logo from '../assets/Logo.svg';
+import greekSalad from '../assets/greekSalad.jpg';
+import resturantFood from '../assets/restauranfood.jpg';
+import lemonDessert from '../assets/lemonDessert.jpg';
+import bruchetta from '../assets/bruchetta.svg';
+import MarioandAdrianA from '../assets/MarioandAdrianA.jpg';
+import MarioandAdrianb from '../assets/MarioandAdrianb.jpg';
+import resturant from '../assets/restaurant.jpg';
+import alexandra from '../assets/alexandra.png';
+import svitlana from '../assets/svitlana.png';
+import valeriia from '../assets/valeriia.png';
+import zhanna from '../assets/zhanna.png';
+import resturantChefB from '../assets/restaurantChefB.jpg';
+import bookingConfirmed from '../assets/bookingConfirmed.png';
+import underConstruction from '../assets/underConstruction.PNG';
+
+export default {logo, greekSalad, resturantFood, lemonDessert, bruchetta, MarioandAdrianA, MarioandAdrianb, resturant, alexandra, svitlana, valeriia, zhanna, resturantChefB, bookingConfirmed, underConstruction};
